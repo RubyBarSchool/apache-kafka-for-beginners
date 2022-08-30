a7cf5b1e4c938148154fcb8399c169393d44a347
Trần Vũ Trường <truongtv1399it@gmail.com>
2022-08-30 16:34:25 +0700

    Upload source code

--- source/kafka-beginners-course/kafka-basic/src/main/java/io/truongtv1399it/demos/kafka/ProducerDemo.java
+++ source/kafka-beginners-course/kafka-basic/src/main/java/io/truongtv1399it/demos/kafka/ProducerDemo.java
@@ -1,9 +1,40 @@
 package io.truongtv1399it.demos.kafka;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
 public class ProducerDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
-
+        log.info("Hello world!");
+        // create Producer Properties
+        Properties properties = new Properties();
+        // template properties
+        // properties.setProperty("key","value");
+        // server kafka
+        // properties.setProperty("bootstrap.server","127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+        // create the Producer
+        KafkaProducer<String,String> producer = new KafkaProducer<>(properties);
+        // create a producer record
+        ProducerRecord<String,String> producerRecord = new ProducerRecord<>("demo_topic","Hello world");
+        // send the data - asynchronous
+        producer.send(producerRecord);
+        // flush data - synchronous
+        producer.flush();
+        // flush and close producer
+        producer.close();
+        // flush and close the Producer
     }
 }
