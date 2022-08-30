1ed3e2c70a05e526fb12e0248840bcf38158bbf7
Trần Vũ Trường <truongtv1399it@gmail.com>
2022-08-30 22:40:31 +0700

    Upload source code Consumer Demo

---
+++ source/kafka-beginners-course/kafka-basic/src/main/java/io/truongtv1399it/demos/kafka/ConsumerDemo.java
@@ -0,0 +1,53 @@
+package io.truongtv1399it.demos.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class ConsumerDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(ConsumerDemo.class);
+
+    public static void main(String[] args) {
+        log.info("I am a Kafka Consumer");
+
+        String bootstrapServer = "127.0.0.1:9092";
+        String groupId = "my-second-application";
+        String autoOffset = "none"; //earliest,latest
+        String topic = "demo_topic";
+
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapServer);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG,groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,autoOffset);
+
+        KafkaConsumer<String,String> consumer = new KafkaConsumer<>(properties);
+
+        consumer.subscribe(Arrays.asList(topic));
+
+        while (true){
+            log.info("Polling");
+
+            ConsumerRecords<String, String> consumerRecord = consumer.poll(Duration.ofMillis(1000));
+
+            for(ConsumerRecord<String,String> record : consumerRecord){
+                log.info("Key: "+record.key() + ", Value: "+record.value());
+                log.info("Partition: "+ record.partition()+", Offset: "+record.offset());
+            }
+        }
+    }
+}
