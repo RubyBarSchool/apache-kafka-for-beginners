de076427bab05541cb464257df4624ba41d83f88
Trần Vũ Trường <truongtv1399it@gmail.com>
2022-09-12 22:55:20 +0700

    Upload package advance

--- source/kafka-beginners-course/kafka-basic/src/main/java/io/truongtv1399it/demos/kafka/ConsumerDemoCooperative.java
+++ source/kafka-beginners-course/kafka-basic/src/main/java/io/truongtv1399it/demos/kafka/ConsumerDemoCooperative.java
@@ -1,9 +1,6 @@
 package io.truongtv1399it.demos.kafka;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.*;
 import org.apache.kafka.common.errors.WakeupException;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
@@ -13,9 +10,9 @@ import java.time.Duration;
 import java.util.Arrays;
 import java.util.Properties;
 
-public class ConsumerDemoWithShutDown {
+public class ConsumerDemoCooperative {
 
-    private static final Logger log = LoggerFactory.getLogger(ConsumerDemoWithShutDown.class);
+    private static final Logger log = LoggerFactory.getLogger(ConsumerDemoCooperative.class);
 
     public static void main(String[] args) {
         log.info("I am a Kafka Consumer");
@@ -35,6 +32,7 @@ public class ConsumerDemoWithShutDown {
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG,groupId);
         properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,autoOffset);
+        properties.setProperty(ConsumerConfig.PARTITION_ASSIGNMENT_STRATEGY_CONFIG, CooperativeStickyAssignor.class.getName());
 
         //create consumer
         KafkaConsumer<String,String> consumer = new KafkaConsumer<>(properties);
